dbda296fedf35aef9b29de0eeef271cdcfdcf350
ldabrowski <lukasz922@gmail.com>
2016-04-21 13:54:19 +0200

    walidacja
    walidacja

--- app/models/rakiety.rb
+++ app/models/rakiety.rb
@@ -1,2 +1,6 @@
 class Rakiety < ActiveRecord::Base
+	validates :marka, :model, presence: true
+	validates :marka, :model, length: { minimum: 3 }
+	validates :marka, length: { maximum: 10 }
+	validates :model, length: { maximum 30 }
 end
